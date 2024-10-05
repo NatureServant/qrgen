3951dcf46fc9758dc6e28fd091fea8561a4f8763
NatureServant <coder01program@outlook.com>
2024-10-05 22:49:45 +0530

    commit-second

--- code.js
+++ code.js
@@ -3,16 +3,19 @@ let resultDiv = document.getElementById('result');
 let qrImage = document.getElementById('qr-image');
 let loadStatement = document.getElementById('load');
 
-function createQrCode() {
+function createQrCode(value) {
+    value = inputBox.value;
     if (inputBox.value !== "") {
         resultDiv.style.display = "flex";
-        qrImage.src = `https://api.qrserver.com/v1/create-qr-code/?data=${inputBox.value}`;
+        qrImage.src = `https://api.qrserver.com/v1/create-qr-code/?data=${value}`;
         loadStatement.innerHTML = inputBox.value;
     }
     else {
         alert("Input field is empty!!")
     }
 }
+inputBox.value = "Conservation of our Mother Earth!!"
+createQrCode(inputBox.value);
 
 inputBox.addEventListener('keypress', function (event) {
     if (event.key === 'Enter') {
